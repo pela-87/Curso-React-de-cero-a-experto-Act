7a7eb2a160ecd45314a82be67bae6e2606cab10c
Jonatan <villarrueljonatan@gmail.com>
2025-09-25 21:12:19 -0300

    Modulo 6 hasta la clase 75.

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { GifList } from './gifs/components/GifList'
 import { PreviousSearches } from './gifs/components/PreviousSearches'
 import { mockGifs } from './mock-data/gifs.mock'
@@ -5,16 +6,37 @@ import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
 
 export const GifsApp = () => {
+
+  const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+
+  const handleTermClicked = (term: string) => {
+    console.log({term});
+  };
+
+  const handleSearch = (query: string = '') => {
+    const maxTerms = 8;
+    
+    query = query.trim().toLowerCase();
+    if (query.length === 0) return;
+    if (previousTerms.includes(query)) return;    
+    setPreviousTerms([query, ...previousTerms].splice(0, maxTerms));    
+  };
+
   return (
     <>
         {/* Header */}
         <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el Gifs perfecto" />
 
         {/* Search */}        
-        <SearchBar title="Buscar" placeholder="Buscar Gifs"/>
+        <SearchBar  
+          placeholder="Buscar Gifs"
+          onQuery= {handleSearch}
+        />
 
         {/* Busquedas previas */}
-        <PreviousSearches searches={['Goku','Saitama']} />
+        <PreviousSearches searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+        />
 
         {/* Gifs */}
         <GifList gifs={mockGifs}/>
--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,13 +1,46 @@
+import { useEffect, useState, type KeyboardEvent } from "react";
+
 interface Props {
-    title: string;
     placeholder?: string;
+
+    onQuery: (query: string) => void;
 }
 
-export const SearchBar = ({ title, placeholder= 'Buscar'}: Props ) => {
+export const SearchBar = ({ placeholder= 'Buscar', onQuery}: Props ) => {
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuery(query);
+      setQuery('');
+    }, 900)
+    
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query, onQuery]);
+
+  const handleSearch = () => {
+    onQuery(query);
+    setQuery('');
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter'){
+      handleSearch();
+    }
+  }
+
   return (
     <div className="search-container">
-        <input type="text" placeholder={placeholder}/>
-        <button>{title}</button>
+        <input 
+          type="text" 
+          placeholder={placeholder}
+          value= {query}
+          onChange={(event) => setQuery(event.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleSearch}>Buscar</button>
     </div>
   )
 }
